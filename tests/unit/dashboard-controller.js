00a2c81ef33877bd7bd6cabce33eb47149f2f602
Michael Strutt <michael@mstrutt.co.uk>
2015-09-02 12:03:19 +0100

    linting fix

--- tests/unit/dashboard-controller.js
+++ tests/unit/dashboard-controller.js
@@ -2,35 +2,35 @@ describe('LoginController', function() {
 	var $controller,
 		rulesData = [
 			{
-				"id": "001",
-				"name": "Highlight expensive products",
-				"last_updated": "2015-08-18T06:36:40+00:00",
-				"last_published": "2015-08-28T16:30:45+00:00"
+				id: "001",
+				name: "Highlight expensive products",
+				last_updated: "2015-08-18T06:36:40+00:00",
+				last_published: "2015-08-28T16:30:45+00:00"
 			}, {
-				"id": "002",
-				"name": "Highlight cheap products",
-				"last_updated": "2015-08-18T06:36:40+00:00",
-				"last_published": "2015-08-28T16:30:45+00:00"
+				id: "002",
+				name: "Highlight cheap products",
+				last_updated: "2015-08-18T06:36:40+00:00",
+				last_published: "2015-08-28T16:30:45+00:00"
 			}, {
-				"id": "003",
-				"name": "Canned results for 'help'",
-				"last_updated": "2015-08-18T06:36:40+00:00",
-				"last_published": "2015-08-28T16:30:45+00:00"
+				id: "003",
+				name: "Canned results for 'help'",
+				last_updated: "2015-08-18T06:36:40+00:00",
+				last_published: "2015-08-28T16:30:45+00:00"
 			}, {
-				"id": "004",
-				"name": "Canned results for 'sale'",
-				"last_updated": "2015-08-18T06:36:40+00:00",
-				"last_published": "2015-08-28T16:30:45+00:00"
+				id: "004",
+				name: "Canned results for 'sale'",
+				last_updated: "2015-08-18T06:36:40+00:00",
+				last_published: "2015-08-28T16:30:45+00:00"
 			}, {
-				"id": "005",
-				"name": "Test promo",
-				"last_updated": "2015-08-18T06:36:40+00:00",
-				"last_published": null
+				id: "005",
+				name: "Test promo",
+				last_updated: "2015-08-18T06:36:40+00:00",
+				last_published: null
 			}, {
-				"id": "006",
-				"name": "Test promo 2",
-				"last_updated": "2015-08-18T06:36:40+00:00",
-				"last_published": null
+				id: "006",
+				name: "Test promo 2",
+				last_updated: "2015-08-18T06:36:40+00:00",
+				last_published: null
 			}
 		],
 		vm;
