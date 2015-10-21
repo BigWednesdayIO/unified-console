797eeb1fd3e9bf126b2f0781f557e70f869765d2
Michael Strutt <michael@mstrutt.co.uk>
2015-10-21 10:00:18 +0100

    removing protractor from tests for now as working with local APIs

--- tests/doubles/rules-data.js
+++ tests/doubles/rules-data.js
@@ -2,22 +2,22 @@ var rulesData = [
 	{
 		id: "001",
 		name: "Published and unpublished match",
-		last_updated: "2015-08-18T06:36:40+00:00",
-		last_published: "2015-08-18T06:36:40+00:00"
+		modified_at: "2015-08-18T06:36:40+00:00",
+		published_at: "2015-08-18T06:36:40+00:00"
 	}, {
 		id: "002",
 		name: "Updated, then published",
-		last_updated: "2015-08-18T06:36:40+00:00",
-		last_published: "2015-08-28T16:30:45+00:00"
+		modified_at: "2015-08-18T06:36:40+00:00",
+		published_at: "2015-08-28T16:30:45+00:00"
 	}, {
 		id: "003",
 		name: "Updated more recently than published",
-		last_updated: "2015-08-18T06:36:40+00:00",
-		last_published: "2015-08-08T16:30:45+00:00"
+		modified_at: "2015-08-18T06:36:40+00:00",
+		published_at: "2015-08-08T16:30:45+00:00"
 	}, {
 		id: "004",
 		name: "Never published",
-		last_updated: "2015-08-18T06:36:40+00:00",
-		last_published: null
+		modified_at: "2015-08-18T06:36:40+00:00",
+		published_at: null
 	}
 ];
